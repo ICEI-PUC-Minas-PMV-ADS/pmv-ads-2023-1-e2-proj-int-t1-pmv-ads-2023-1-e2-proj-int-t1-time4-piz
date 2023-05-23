351ced8da3b91acf68e8b2d47fc3e539dce0b6f6
Leticia Pauli <leh_pauli9@hotmail.com>
2023-05-22 21:20:20 -0300

    Adicionando areas/funcionarios

---
+++ src/PizzApp/PizzApp/Areas/Funcionarios/Controllers/FuncionariosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PizzApp.Areas.Funcionarios.Controllers
+{
+    [Area("Funcionarios")]
+    [Authorize("Funcionarios")]
+    public class FuncionariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
